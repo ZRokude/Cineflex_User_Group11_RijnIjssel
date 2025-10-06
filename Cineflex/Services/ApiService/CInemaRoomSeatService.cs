ce33de85c828f645e1fb713675e30cd034999a5a
ZRoku <ZRoku89@gmail.com>
2025-10-06 10:10:17 +0200

    add cinemaroomseatservice

--- Cineflex/Services/ApiService/CInemaRoomSeatService.cs
+++ Cineflex/Services/ApiService/CInemaRoomSeatService.cs
@@ -1,11 +1,21 @@
-﻿using Cineflex.Utilities;
+﻿using Cineflex.Models;
+using Cineflex.Utilities;
+using Cineflex_API.Model.Commands.Cinema;
+using Cineflex_API.Model.Responses.Cinema;
 
 namespace Cineflex.Services.ApiService
 {
     public interface ICinemaRoomSeatService
     {
+        public Task<ModelServiceResponse<IEnumerable<CinemaRoomSeatResponse>>> Get(Guid Id);
     }
-    public class CinemaRoomSeatService
-
+    public class CinemaRoomSeatService(HttpRequestHandler<Program> requestHandler, NotifyService notifyService)
+        :BaseApiService(requestHandler, notifyService)
+        ,ICinemaRoomSeatService
+    { 
+        public async Task<ModelServiceResponse<IEnumerable<CinemaRoomSeatResponse>>> Get(Guid Id)
+        {
+            return await requestHandler.GetAsync<IEnumerable<CinemaRoomSeatResponse>>($"/CinemaRoomSeat?id={Id}", CancellationToken.None);
+        }
     }
 }
