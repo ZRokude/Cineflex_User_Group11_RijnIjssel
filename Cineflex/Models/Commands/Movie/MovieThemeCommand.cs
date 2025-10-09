d045050f3d2749505694d8f1b7d57f1b3f2f29bf
ZRoku <ZRoku89@gmail.com>
2025-10-09 10:47:19 +0200

    update genrecommands and genreresponse. Add theme,movietheme commands and responses

---
+++ Cineflex/Models/Commands/Movie/MovieThemeCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Cineflex.Models.Commands.Movie
+{
+    public class MovieThemeCommand
+    {
+        public Guid MovieId { get; set; } = Guid.Empty;
+        public Guid GenreId { get; set; } = Guid.Empty;
+    }
+}
---
+++ Cineflex/Models/Responses/Movie/MovieThemeResonse.cs
@@ -0,0 +1,8 @@
+﻿namespace Cineflex.Models.Responses.Movie
+{
+    public class MovieThemeResonse
+    {
+        public Guid MovieId { get; set; } = Guid.Empty;
+        public Guid GenreId { get; set; } = Guid.Empty;
+    }
+}
